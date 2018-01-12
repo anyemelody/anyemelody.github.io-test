87c5fb1f85f35ed0e4a08377a084ad2a719f5a9f
anyemelody <anyemelody@gmail.com>
2018-01-12 12:27:40 -0500

    succeed array

--- sketch.js
+++ sketch.js
@@ -1,36 +1,44 @@
 var scene = document.querySelector('a-scene'); //grab the scene element
 var angle = Math.random(1);
-var ball, posX, posY, posZ;
+var posX, posY, posZ;
 var balls = [];
 // var ball = document.querySelector('testBall');
 
 function setup() {
-  for (var i = 0; i < 10; i++) {
-    ball = document.createElement('a-sphere');
+  for (var i = 0; i < 100; i++) {
+    var ball = document.createElement('a-sphere');
+    posX = random(-5, 5);
+    posY = random(-5, 5);
+    posZ = random(-5, 5);
     ball.setAttribute('position', {
-      x: random(-5, 5),
-      y: random(0, 5),
-      z: random(-5, 5)
+      x: posX,
+      y: posY,
+      z: posZ
     });
     ball.setAttribute('radius', 0.4);
     ball.setAttribute('color', 'red');
     // ball.setAttribute('dynamic-body', true);
     ball.setAttribute('material', {
-      color: 'white',
-      metalness: 0.5
+      color: 'red',
+      metalness: 0.7
     });
-    scene.appendChild(ball);
-    balls.push(ball);
+    balls[i] = ball;
+    scene.appendChild(balls[i]);
+    ball.myPosX = posX;
+    ball.myPosY = posY;
+    ball.myPosZ = posZ;
   }
 }
 
 function draw() {
-  posY += sin(angle) * 0.1;
   angle += 0.1;
-  for (var i = 0; i < 1; i++) {
-    var item = balls[i];
-    item.setAttribute('position', {
-      y: posY
+  var timer = 0.0001*Date.now();
+  for (var i = 0; i < balls.length; i++) {
+    var sphere = balls[i];
+    sphere.setAttribute('position', {
+      y: sphere.myPosY*sin(timer+i),
+      x: sphere.myPosX*cos(timer+i*1.1),
+      z: sphere.myPosZ
     });
   }
 };
